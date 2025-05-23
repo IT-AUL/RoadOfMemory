35f73d31d3cc944534650a777725b7b17b18f256
DanisGaleev <galeevdanisgdr@gmail.com>
2025-05-23 15:01:45 +0300

    [*] cors config [*] fixed bug with swagger

--- src/main/java/com/itaul/rofm/config/CorsConfig.java
+++ src/main/java/com/itaul/rofm/config/CorsConfig.java
@@ -10,7 +10,8 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("*")
+//                .allowedOriginPatterns("*")
+                .allowedOrigins("localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
